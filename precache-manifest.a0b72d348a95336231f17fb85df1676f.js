a2d2ca4b39afe3f35338a8a5941bb9ba12d41f62
NADEEM <sayyadnad8126@gmail.com>
2020-12-26 01:02:06 +0530

    Updates

---
+++ precache-manifest.a0b72d348a95336231f17fb85df1676f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a0af555c6eb0f7851a6fe0187f5e5d",
+    "url": "/Expense-Manager/index.html"
+  },
+  {
+    "revision": "80854d94722d66832067",
+    "url": "/Expense-Manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "80854d94722d66832067",
+    "url": "/Expense-Manager/static/js/2.1814e6a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Manager/static/js/2.1814e6a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3ec5d78660da2f83a6f",
+    "url": "/Expense-Manager/static/js/main.98afec30.chunk.js"
+  },
+  {
+    "revision": "4a9c486ca1eb2fadab49",
+    "url": "/Expense-Manager/static/js/runtime-main.5ac7e3d3.js"
+  }
+]);
